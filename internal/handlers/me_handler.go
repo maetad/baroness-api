fa752697778fc9194df6f957defa285f98ac611f
Maetad Sukarasud <maetad91@gmail.com>
2022-07-30 09:33:29 +0700

    feat(auth): :sparkles: update current user

--- internal/handlers/me_handler.go
+++ internal/handlers/me_handler.go
@@ -9,11 +9,15 @@ import (
 )
 
 type MeHandler struct {
-	log *logrus.Entry
+	log         *logrus.Entry
+	userservice userservice.UserServiceInterface
 }
 
-func NewMeHandler(log *logrus.Entry) *MeHandler {
-	return &MeHandler{log}
+func NewMeHandler(
+	log *logrus.Entry,
+	userservice userservice.UserServiceInterface,
+) *MeHandler {
+	return &MeHandler{log, userservice}
 }
 
 func (h *MeHandler) Get(c *gin.Context) {
@@ -29,4 +33,32 @@ func (h *MeHandler) Get(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, user)
+}
+
+func (h *MeHandler) Update(c *gin.Context) {
+	var (
+		user *userservice.User
+		r    userservice.UserUpdateRequest
+		ok   bool
+	)
+
+	if user, ok = c.MustGet("user").(*userservice.User); !ok {
+		h.log.Error(`Delete(): c.MustGet("user") is not *userservice.User`)
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if err := c.ShouldBindJSON(&r); err != nil {
+		c.AbortWithStatus(http.StatusUnprocessableEntity)
+		return
+	}
+
+	u, err := h.userservice.Update(user, r)
+	if err != nil {
+		h.log.WithError(err).Errorf("Update(): h.userservice.Update error %v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, u)
 }
